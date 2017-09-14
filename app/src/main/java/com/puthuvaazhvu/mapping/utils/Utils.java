6d2821d317682a3aa9bf6151d86ded660b726a84
swaaminathanm <swaaminathanm@gmail.com>
2017-09-14 18:08:24 +0530

    Initial commit in this branch

---
+++ app/src/main/java/com/puthuvaazhvu/mapping/utils/Utils.java
@@ -0,0 +1,13 @@
+package com.puthuvaazhvu.mapping.utils;
+
+import java.util.UUID;
+
+/**
+ * Created by muthuveerappans on 9/14/17.
+ */
+
+public class Utils {
+    public static String generateRandomUUID() {
+        return String.valueOf(UUID.randomUUID());
+    }
+}
